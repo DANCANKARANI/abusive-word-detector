4a965a9ceb6ee9d712b19ba96bf94979a90a8c8b
DANCAN KARANI <karanidancan120@gmail.com>
2024-07-21 08:41:59 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	 "github.com/TwiN/go-away"
+)
+type Message struct{
+	Text string `json:"text"`
+	Sender string `json:"sender"`
+}
+func main() {
+	fmt.Println("Welcome....")
+	abusive,_:=DetectAbusive("fuck this asshole")
+	fmt.Println(abusive)
+	app:=fiber.New()
+	app.Post("api/v1/message",DetectAbusiveHandler)
+	app.Listen(":3000")
+}
+func DetectAbusiveHandler(c *fiber.Ctx)error{
+	message :=new(Message)
+	if err := c.BodyParser(message); err != nil{
+		return c.JSON(fiber.Map{"error":"failed to parse json data"})
+	}
+	isAbusive,abusiveWord:=DetectAbusive(message.Text)
+	if isAbusive{
+		return c.JSON(fiber.Map{"abusive word:":abusiveWord})
+	}
+	return nil
+}
+func DetectAbusive(message string)(bool,string){
+	if goaway.IsProfane(message){
+		abusive:=goaway.ExtractProfanity(message)
+		return true,abusive
+	}
+	return false,""
+}
